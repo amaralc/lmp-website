b6d51a9afb018bbe56312e212af9e18b2c301b43
amaralc <amaral.calil@gmail.com>
2019-08-09 02:38:28 -0300

    Increased height of carousel to 400px

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -17,24 +17,21 @@ export default function Carousel() {
                     <img className="img-fluid" src={iconLaser} alt="First slide" />
                     <div className="container carousel-caption d-md-block">
                         <a href=""><h5>Laser Processing</h5></a>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum lacinia lorem.<br /> Mauris varius scelerisque metus vel ultrices. Aliquam convallis.</p>
-                        <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                        <p>Description</p>                        
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="img-fluid" src={iconMachining} alt="Second slide" />
                     <div className="container carousel-caption d-md-block">
                         <a href=""><h5>Machining</h5></a>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Vivamus sed venenatis ex. Vestibulum ante ipsum.</p>
-                        <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                        <p>Description</p>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="img-fluid" src={iconGrinding} alt="Third slide" />
                     <div className="container carousel-caption d-md-block">
                         <a href=""><h5>Grinding</h5></a>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Phasellus ultricies iaculis arcu. Nunc posuere elit sed metus luctus, et posuere mauris hendrerit. Cras mollis magna.</p>
-                        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                        <p>Description</p>                        
                     </div>
                 </div>
             </div>
