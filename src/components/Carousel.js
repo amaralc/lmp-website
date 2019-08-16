d633bdf7de12ce9c01b16f68478cc0331a920aae
amaralc <amaral.calil@gmail.com>
2019-08-15 21:53:14 -0300

    style: adjust carousel size and position

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,7 +6,7 @@ import iconGrinding from "../img/icon_grinding.png"
 
 export default function Carousel() {
     return (
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" className="carousel slide mb-5 pt-5 pb-5" data-ride="carousel">
             <ol className="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -15,21 +15,21 @@ export default function Carousel() {
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img className="img-fluid" src={iconLaser} alt="First slide" />
-                    <div className="container carousel-caption d-md-block">
+                    <div className="container carousel-caption d-none d-md-block">
                         <a href=""><h5>Laser Processing</h5></a>
                         <p>Description</p>                        
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="img-fluid" src={iconMachining} alt="Second slide" />
-                    <div className="container carousel-caption d-md-block">
+                    <div className="container carousel-caption d-none d-md-block">
                         <a href=""><h5>Machining</h5></a>
                         <p>Description</p>
                     </div>
                 </div>
                 <div className="carousel-item">
                     <img className="img-fluid" src={iconGrinding} alt="Third slide" />
-                    <div className="container carousel-caption d-md-block">
+                    <div className="container carousel-caption d-none d-md-block">
                         <a href=""><h5>Grinding</h5></a>
                         <p>Description</p>                        
                     </div>
