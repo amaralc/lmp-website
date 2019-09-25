1554c00e67636c782f7edf4079522941f92a4cc1
Denise Albertazzi Gonçalves <denise.albertazzi@gmail.com>
2019-09-25 17:45:40 -0300

    Cleanup, path correction and box-size styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer";
+
+import "./App.css";
 
 export default class App extends React.Component {
   render() {
     return (
-      <div> 
-        <NavBar/>       
+      <div>
+        <NavBar />
         <main>{this.props.children}</main>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
