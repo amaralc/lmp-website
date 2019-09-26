2f260e109f8c1d5ff9025a3becc7b4debab1dff5
Denise Albertazzi Gonçalves <denise.albertazzi@gmail.com>
2019-09-25 22:47:21 -0300

    Changed footer background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ import { Navbar } from "react-bootstrap";
 export class Footer extends Component {
   render() {
     return (
-      <Navbar sticky="bottom" bg="dark">
+      <Navbar
+        fixed="bottom"
+        style={{ backgroundColor: "var(--lmp-dark-blue)" }}
+      >
         <Navbar.Text className="mx-auto text-white">
           Copyright &copy; LMP 2019
         </Navbar.Text>
